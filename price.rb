9f19e3ffee7af9c0dc3d37a401a934c78ccb5912
James Loosli <james@instantchannelinc.com>
2014-06-30 18:03:03 -0700

    This is broken

--- price.rb
+++ price.rb
@@ -4,12 +4,16 @@ require 'rest_client'
 require 'optparse'
 require 'nokogiri'
 require 'yaml'
+require 'pp'
 
 def parse_options
   options = {}
   optparse = OptionParser.new do |opts|
     opts.banner = "Usage: #{__FILE__} [options]"
-    opts.on('-l LINK','--link LINK','Provide the link to search') do |l|
+    opts.on('-f FILE','--file FILE','Provide a file with multiple links, one per line') do |f|
+      options[:file] = f
+    end
+     opts.on('-l LINK','--link LINK','Provide the link to search') do |l|
       options[:link] = l
     end
     opts.on('-v','--verbose','Run verbosely') do |v|
@@ -23,10 +27,12 @@ def parse_options
 
   begin
     optparse.parse!
-    mandatory = [:link]
-    missing = mandatory.select{ |param| options[param].nil? }
-    if not missing.empty?
-      puts "Missing options: #{missing.join(', ')}"
+    if options[:link].nil? && options[:file].nil? 
+      puts "Please specify a link or a file to check, but not both."
+      puts optparse
+      exit
+    elsif !options[:link].nil? && !options[:file].nil?
+      puts "Please specify a link or a file to check, but not both."
       puts optparse
       exit
     end
@@ -64,21 +70,40 @@ def get_price(page, path)
   return price
 end
 
+def link_to_price(link, tags, verbose)
+  domain = domain_str(link)
+
+  puts tags[domain]
+
+#  return "No template for domain: #{domain}" unless tags.has_key?("#{domain}")
+#  puts "Domain is #{domain}" if verbose
+#  puts "Getting link" if verbose
+#  page = get_link(link)
+#  puts "Parsing HTML for price" if verbose
+#  price = get_price(page, tags[domain])
+
+#  return price.to_s
+end
+
 if __FILE__ == $0
   #do work
   #init some stuff
   options = parse_options
+  puts "Loading tags" if options[:verbose]
   tags = parse_tags
+  puts "Tags loaded;" if options[:verbose]
+  pp tags if options[:verbose]
 
-  domain = domain_str(options[:link])
-  puts "Domain is #{domain}" if options[:verbose]
-
-  puts "Getting link" if options[:verbose]
-  page = get_link(options[:link])
-
-  puts "Parsing HTML for price" if options[:verbose]
-  price = get_price(page, tags[domain])
-
-  puts price.to_s.split.join(" ")
-
+  if options[:link].nil?
+    f = File.open(options[:file])
+    f.each_line do |link|
+      puts "Checking link: #{link}" if options[:verbose]
+      val =  link_to_price(link, tags, options[:verbose])
+      puts val
+    end
+  else
+    puts "Checking link: #{options[:link]}" if options[:verbose]
+    val = link_to_price(options[:link],tags,options[:verbose])
+    puts val
+  end
 end
